9b2d24d04a8feb069af190e518f41a1b484d2d38
Peter Donovan <managingwholes.com@gmail.com>
2020-05-12 05:32:28 -0700

    slow changes

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button style="font-size:24px">
+      <i class="fa fa-home">Home</i>
+    </button>
+    <h1>Login</h1>
+    <p>User will login to a PROJECT</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Outbox.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Outbox</h1>
+    <p>This will show a list of outbox objects to sync or upload</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
